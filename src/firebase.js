e5db890f90c484cdb8a113fd78c0cec34de78eed
Nouman Javed <noumanmalik960@gmail.com>
2020-11-08 15:50:52 +0500

    added files

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,13 @@ import firebase from 'firebase'
 
 
 const config = {
-  // keys hidden for github
+  apiKey: "AIzaSyBMRjQ_B00edGPNMzlt8mdE4U_hHLIM0nQ",
+  authDomain: "expense-tracker-80959.firebaseapp.com",
+  databaseURL: "https://expense-tracker-80959.firebaseio.com",
+  projectId: "expense-tracker-80959",
+  storageBucket: "expense-tracker-80959.appspot.com",
+  messagingSenderId: "50076654142",
+  appId: "1:50076654142:web:6838101034c5e8552b8f6f"
 }
 
 
